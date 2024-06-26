da19778705bac9ce6e9ae69884e7d09262de1477
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-06-26 05:26:59 -0300

    greetingPrefix

--- tests/hello/hello.go
+++ tests/hello/hello.go
@@ -2,15 +2,53 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
-		name = "World"
+		name = "Worl"
 	}
-	return helloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
+
+/*
+
+A few new concepts:
+
+In our function signature we have made a named return value (prefix string).
+
+This will create a variable called prefix in your function.
+It will be assigned the "zero" value. This depends on the type, for example ints are 0 and for strings it is "".
+You can return whatever it's set to by just calling return rather than return prefix.
+This will display in the Go Doc for your function so it can make the intent of your code clearer.
+
+default in the switch case will be branched to if none of the other case statements match.
+
+The function name starts with a lowercase letter. In Go, public functions start with a capital letter, and private ones start with a lowercase letter. We don't want the internals of our algorithm exposed to the world, so we made this function private.
+
+Also, we can group constants in a block instead of declaring them on their own line. For readability, it's a good idea to use a line between sets of related constants.
+
+*/
